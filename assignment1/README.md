24783defc1d8ad038798f75696248e4f4b824695
Mere19 <mere1998@outlook.com>
2022-03-11 09:01:59 +0100

    Solution assignment 1

--- assignment1/README.md
+++ assignment1/README.md
@@ -21,27 +21,53 @@ Edit this 'README.md' file to report all your results. You only need to update t
 ### text dump of the data structure for "plane.off"
 |Vertices-to-Face      | Vertices-to-Vertex  |
 | :------------------- |:------------------- |
-| paste something here | paste something here|
-
+| vertex 0: 5 21 |  vertex 0: 11 13 22 |
+vertex 1: 31 | vertex 1: 9 12 |
+vertex 2: 10 26 | vertex 2: 10 15 23 |
+vertex 3: 0 | vertex 3: 14 16 |
+vertex 4: 14 27 30 | vertex 4: 9 10 20 24 |
+vertex 5: 13 23 29 | vertex 5: 11 12 19 24 |
+vertex 6: 1 4 17 | vertex 6: 13 14 18 21 |
+vertex 7: 2 8 18 | vertex 7: 15 16 17 21 |
+vertex 8: 6 9 12 19 22 25 | vertex 8: 17 18 19 20 22 23 |
+vertex 9: 15 30 31 | vertex 9: 1 4 12 24 |
+vertex 10: 11 26 27 | vertex 10: 2 4 20 23 |
+vertex 11: 7 21 23 | vertex 11: 0 5 19 22 |
+vertex 12: 15 29 31 | vertex 12: 1 5 9 24 |
+vertex 13: 4 5 20 | vertex 13: 0 6 18 22 |
+vertex 14: 0 1 16 | vertex 14: 3 6 16 21 |
+vertex 15: 8 10 24 | vertex 15: 2 7 17 23 |
+vertex 16: 0 2 16 | vertex 16: 3 7 14 21 |
+vertex 17: 3 8 9 18 19 24 | vertex 17: 7 8 15 18 21 23 |
+vertex 18: 3 4 6 17 19 20 | vertex 18: 6 8 13 17 21 22 |
+vertex 19: 7 12 13 22 23 28 | vertex 19: 5 8 11 20 22 24 |
+vertex 20: 11 12 14 25 27 28 | vertex 20: 4 8 10 19 23 24 |
+vertex 21: 1 2 3 16 17 18 | vertex 21: 6 7 14 16 17 18 |
+vertex 22: 5 6 7 20 21 22 | vertex 22: 0 8 11 13 18 19 |
+vertex 23: 9 10 11 24 25 26 | vertex 23: 2 8 10 15 17 20 |
+vertex 24: 13 14 15 28 29 30 |vertex 24: 4 5 9 12 19 20 |
 
 
 ### Show three screenshots of the 'fandisk.off' model using different shading. Make sure you disable the wireframe and that the screenshots clearly show the differences between the different shading types.
 | model name  | per-face shading    | per-vertex shading |  per-corner shading |
 | :---------: | ------------------- | ------------------ | ------------------- |
-| fandisk     |<img align="center" src="./res/placeholder.png" width="300">| <img align="center"  src="./res/placeholder.png" width="300"> |<img align="center"  src="./res/placeholder.png" width="300">  |
+| fandisk     |<img align="center" src="./res/Q4_perf.png" width="300">| <img align="center"  src="./res/Q4_perv.png" width="300"> |<img align="center"  src="./res/Q4_perc.png" width="300">  |
 
 #### Briefly describe the difference between the different shading types.
-Explain here.
+In comparison with the other two, the edges of the per-vertex shading model are more blurred. In addition, compared to the per-face shading model, specular highlights of the per-vertex shading model are rendered better.\
+In comparison to the other two, the angles between the faces of the per-face shading model are more obvious.\
+In comparison to per-vertex shading, the edges of the per-corner shading is sharper.
+
+
 
 ### Assign different colors to each connected component
+
 | model name   | your results                                                  |  no. of components | no. of faces per component |
 | :----------: | ------------------------------------------------------------  |:-----------------: | :------------------------: |
-|bumpy_cube    |<img align="center"  src="./res/placeholder.png" width="300">  |   2                |  2000/3000                 |
-|bunny         |<img align="center"  src="./res/placeholder.png" width="300">  |   x                |  xxxxxxxxx                 |
-|coffeecup     |<img align="center"  src="./res/placeholder.png" width="300">  |   x                |  xxxxxxxxx                 |
-|honda         |<img align="center"  src="./res/placeholder.png" width="300">  |   x                |  xxxxxxxxx                 |
-
-
+|bumpy_cube    |<img align="center"  src="./res/bumpy_cube.png" width="300">  |   1                |  2496               |
+|bunny         |<img align="center"  src="./res/bunny.png" width="300">  |   1                |  27864                 |
+|coffeecup     |<img align="center"  src="./res/coffeecup.png" width="300">  |   2                | 3360/2304               |
+|honda         |<img align="center"  src="./res/honda.png" width="300">  |   11               | 90/192/192/13216/704/1088/1088/1088/1088/736/736 |
 
 ### Screenshots of subdivided meshes. Make sure you enable the wireframe overlay.
 
--- assignment1/src/main.cpp
+++ assignment1/src/main.cpp
@@ -5,6 +5,13 @@
 #include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
 #include <imgui/imgui.h>
 /*** insert any libigl headers here ***/
+#include <igl/vertex_triangle_adjacency.h>
+#include <igl/adjacency_list.h>
+#include <igl/per_face_normals.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/per_corner_normals.h>
+#include <igl/facet_components.h>
+#include <igl/jet.h>
 
 using namespace std;
 using Viewer = igl::opengl::glfw::Viewer;
@@ -30,7 +37,51 @@ void subdivide_sqrt3(const Eigen::MatrixXd &V,
 					 const Eigen::MatrixXi &F,
 					 Eigen::MatrixXd &Vout,
 					 Eigen::MatrixXi &Fout){
-	
+    // copy old vertices to Vout
+    for (int i = 0; i < V.rows(); i ++) {
+        Vout(i) = V(i);
+    }
+    std::cout << Vout << std::endl;
+    // copy old faces to Fout
+    for (int i = 0; i < F.rows(); i ++) {
+        Fout(i) = F(i);
+    }
+    std::cout << Fout << std::endl;
+
+    // locate new vertices
+    int j = V.rows();
+    int k = F.rows();
+    for (int i = 0; i < F.rows(); i ++) {
+        double x = (V(F(i, 0), 0) + V(F(i, 1), 0) + V(F(i, 2), 0)) / 3;
+        double y = (V(F(i, 0), 1) + V(F(i, 1), 1) + V(F(i, 2), 1)) / 3;
+        double z = (V(F(i, 0), 2) + V(F(i, 1), 2) + V(F(i, 2), 2)) / 3;
+
+        // add new vertex to Vout
+        Vout(j, 0) = x;
+        Vout(j, 1) = y;
+        Vout(j, 2) = z;
+        std::cout << Vout(j) << std::endl;
+        j ++;
+
+        std::cout << F(i, 0) << " " << F(i, 1) << " " << F(i, 2) << std::endl;
+
+        // add new faces to Fout
+        Fout(k, 0) = F(i, 0);
+        Fout(k, 1) = F(i, 1);
+        Fout(k++, 2) = j - 1;
+        std::cout << Fout(k - 1, 0) << Fout(k - 1, 1) << Fout(k - 1, 2) << std::endl;
+
+        Fout(k, 0) = F(i, 1);
+        Fout(k, 1) = F(i, 2);
+        Fout(k++, 2) = j - 1;
+        std::cout << Fout(k - 1, 0) << Fout(k - 1, 1) << Fout(k - 1, 2) << std::endl;
+
+        Fout(k, 0) = F(i, 2);
+        Fout(k, 1) = F(i, 0);
+        Fout(k++, 2) = j - 1;
+        std::cout << Fout(k - 1, 0) << Fout(k - 1, 1) << Fout(k - 1, 2) << std::endl;
+
+    }
 }
 
 bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers) {
@@ -39,6 +90,14 @@ bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers) {
         viewer.data().set_mesh(V, F);
         // Add your code for computing vertex to face relations here;
         // store in VF,VFi.
+        igl::vertex_triangle_adjacency(V, F, VF, VFi);
+        for (int i = 0; i < VF.size(); i++) {
+            std::cout << "| vertex " << i << ": ";
+            for (int fidx : VF[i]) {
+                std::cout << fidx << " ";
+            }
+            std::cout << "|" << std::endl;
+        }
     }
 
     if (key == '2') {
@@ -46,6 +105,14 @@ bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers) {
         viewer.data().set_mesh(V, F);
         // Add your code for computing vertex to vertex relations here:
         // store in VV.
+        igl::adjacency_list(F, VV);
+        for (int i = 0; i < VV.size(); i++) {
+            std::cout << "| vertex " << i << ": ";
+            for (int vidx : VV[i]) {
+                std::cout << vidx << " ";
+            }
+            std::cout << "|"<< std::endl;
+        }
     }
 
     if (key == '3') {
@@ -53,7 +120,7 @@ bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers) {
         viewer.data().set_mesh(V, F);
         FN.setZero(F.rows(),3);
         // Add your code for computing per-face normals here: store in FN.
-
+        igl::per_face_normals(V, F, FN);
         // Set the viewer normals.
         viewer.data().set_normals(FN);
     }
@@ -62,35 +129,53 @@ bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers) {
         viewer.data().clear();
         viewer.data().set_mesh(V, F);
         // Add your code for computing per-vertex normals here: store in VN.
-
+        igl::per_vertex_normals(V, F, VN);
         // Set the viewer normals.
+        viewer.data().set_normals(VN);
     }
 
     if (key == '5') {
         viewer.data().clear();
         viewer.data().set_mesh(V, F);
         // Add your code for computing per-corner normals here: store in CN.
-
+        igl::per_corner_normals(V, F, 75, CN);
         //Set the viewer normals
+        viewer.data().set_normals(CN);
     }
 
     if (key == '6') {
         viewer.data().clear();
         viewer.data().set_mesh(V, F);
         component_colors_per_face.setZero(F.rows(),3);
+
         // Add your code for computing per-face connected components here:
         // store the component labels in cid.
+        igl::facet_components(F, cid);
+        int max = 0;
+        for (int i = 0; i < cid.size(); i ++) {
+            if (cid[i] > max) max = cid[i];
+        }
+        std::cout << "number of components: " << max + 1 << std::endl;
+        std::vector<int> count_component(max + 1, 0);
+        for (int i = 0; i < cid.size(); i ++) {
+            count_component[cid[i]] ++;
+        }
+        for (int i = 0; i < count_component.size(); i ++) {
+            std::cout << "component " << i << ": " << count_component[i] << std::endl;
+        }
 
         // Compute colors for the faces based on components, storing them in
         // component_colors_per_face.
-
+        igl::jet(cid, true, component_colors_per_face);
         // Set the viewer colors
         viewer.data().set_colors(component_colors_per_face);
     }
 
     if (key == '7') {
-		Eigen::MatrixXd Vout;
-		Eigen::MatrixXi Fout;
+        // std::cout << "in 7" << std::endl;
+		Eigen::MatrixXd Vout(V.rows() + F.rows(), 3);
+		Eigen::MatrixXi Fout(3 * F.rows(), 3);
+        // Eigen::MatrixXi Fout;
         // Fill the subdivide_sqrt3() function with your code for sqrt(3) subdivision.
 		subdivide_sqrt3(V,F,Vout,Fout);
         // Set up the viewer to display the new mesh
@@ -127,7 +212,7 @@ int main(int argc, char *argv[]) {
 	
     load_mesh(viewer,filename,V,F);
 
-    callback_key_down(viewer, '1', 0);
+    callback_key_down(viewer, '2', 0);
 
     // Attach a menu plugin
     igl::opengl::glfw::imgui::ImGuiMenu menu;
